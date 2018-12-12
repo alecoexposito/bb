dada96c2239a2212d169137a2c84add05e92bf75
alecoexposito <alecoexposito@gmail.com>
2018-12-12 10:35:05 -0500

    changes for bb streaming

--- worker.js
+++ worker.js
@@ -15,28 +15,46 @@ class Worker extends SCWorker {
     run() {
         console.log('   >> Worker PID:', process.pid);
         var scServer = this.scServer;
-        var bb = require(__dirname + '/BBCtrl')(SerialPort, nmea, net,fs, Readline, scServer);
-        bb.run({'serialPort':'/dev/ttyS1','baudRate':9600,'port':3002,'ipAddress':'192.168.1.100'});
-        scServer.on('connection', function(socket) {
-		console.log("on connection: ", socket);
-	});
-	
-	            var options = {
-			                secure: false,
-			                hostname: '192.168.1.100',
-			                port: 3001
-			            };
+        var bb = require(__dirname + '/BBCtrl')(SerialPort, nmea, net, fs, Readline, scServer);
+        bb.run({'serialPort': '/dev/ttyS1', 'baudRate': 9600, 'port': 3002, 'ipAddress': '192.168.1.100'});
+        scServer.on('connection', function (socket) {
+            console.log("on connection: ", socket);
+        });
+
+        var options = {
+            secure: false,
+            hostname: '192.168.1.100',
+            port: 3001
+        };
         var socket = socketClient.connect(options);
-	            socket.on('connect', function () {
-			                console.log("conectado al server websocket del tracker");
-			            });
-	            var cameraChannel = socket.subscribe('camera_channel');
-	            cameraChannel.watch(function(data) {
-			                console.log("recibido del tracker: ", data);
-			            });
+        socket.on('connect', function () {
+            console.log("conectado al server websocket del tracker");
+        });
+        var cameraChannel = socket.subscribe('camera_channel');
+        cameraChannel.watch(function (data) {
+            if (data.type == "start-streaming") {
+                console.log('starting streaming');
+                const
+                    {spawn} = require('child_process'),
+                    ls = spawn('ls', ['-lh', '/usr']);
+
+                ls.stdout.on('data', data => {
+                    console.log(`stdout: ${data}`);
+                });
+
+                ls.stderr.on('data', data => {
+                    console.log(`stderr: ${data}`);
+                });
+
+                ls.on('close', code => {
+                    console.log(`child process exited with code ${code}`);
+                });
+            }
+        });
 
 
     }
 }
+
 new Worker();
 

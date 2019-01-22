4f9ab866aa37c4bcd2f899b6615c9970a407d981
Alejandro <alecoexposito@gmail.com>
2019-01-22 11:17:24 -0500

    adding script to execute at bb startup

--- scripts/bb-init.sh
+++ scripts/bb-init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sshfs -p 33 zurikato@187.162.125.161:/var/www/html/cameras/386 /home/zurikato/camera
-gst-launch-1.0 -v -e rtspsrc protocols=tcp location="rtsp://192.168.1.16:554/user=admin&password=&channel=1&stream=1.sdp" ! queue ! rtph264depay ! h264parse config-interval=-1 ! mpegtsmux ! hlssink location="/home/zurikato/camera/hls%05d.ts" target-duration=1
+ffmpeg -rtsp_flags prefer_tcp -i "rtsp://192.168.1.16:554/user=admin&password=&channel=1&stream=1.sdp" -acodec copy -vcodec copy -f segment -segment_list /home/zurikato/video-backup/playlist.m3u8 -segment_time 30 -strftime 1 "/home/zurikato/video-backup/%Y-%m-%d_%H-%M-%S_hls.ts"

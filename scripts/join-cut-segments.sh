2a75171adf8748f199b4a82ad564abcd5d10cc80
Alejandro <alecoexposito@gmail.com>
2019-02-12 01:08:23 -0500

    adding functionalities for download part of video

--- scripts/join-cut-segments.sh
+++ scripts/join-cut-segments.sh
@@ -3,5 +3,6 @@ startTime=$1
 totalSeconds=$2
 folderPath=/home/zurikato/camera/video/$3
 cd $folderPath
+ls *.ts > videos.txt
 ffmpeg -safe 0 -f concat -n -i videos.txt -c copy videos.mp4
 ffmpeg -ss $startTime -t $totalSeconds -y -i videos.mp4 download.mp4

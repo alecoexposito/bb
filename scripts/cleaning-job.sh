68d7858797678d6fe83892ec45cc8840f42dce2f
Alejandro <alecoexposito@gmail.com>
2019-02-15 11:14:37 -0500

    adding functionalities for download part of video

---
+++ scripts/cleaning-job.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+filename="$1"
+name=""
+while read -r line; do
+    if [[ $line == *.ts ]]
+    then
+	    name=$line
+	    echo "hasta aqui las clases: $name"
+	    break
+    fi
+    echo "ignored: $line"
+done < "$filename"
+
+for f in *.ts; do
+    if [[ "$f" < "$name" ]]
+    then
+    	rm $f
+    	echo deleted: $f
+    else
+    	break
+    fi
+done

cd332aaf8124db33c48da2ef20bb6932b4459f20
Alejandro <alecoexposito@gmail.com>
2019-03-08 12:57:58 -0500

    adding functionalities for download part of video

--- BBCtrl.js
+++ BBCtrl.js
@@ -4,26 +4,33 @@ module.exports = (SerialPort, nmea, net, fs, Readline, scServer) => {
             var sqlite3 = require('sqlite3').verbose();
             this.db = new sqlite3.Database('/home/zurikato/.db/bb.sqlite', sqlite3.OPEN_READWRITE, (err) => {
                 if(err) {
-                    console.log("error openning the sqlite database");
+                    return console.log("error openning the sqlite database");
                 }
                 console.log('connected to the sqlite database');
             });
 
         }
 
-        saveOfflineData() {
+        saveOfflineData(values) {
             var db = this.db;
+            db.run('insert into info_data(lat, lng, speed, created_at, updated_at) values(?)', values, function(err) {
+                if(err) {
+                    return console.log(console.log(err.message));
+                }
 
+                console.log('Row inserted with id: ', this.lastID);
+            });
 
         }
 
         run(options) {
+            var self = this;
 
             /*let content = fs.readFileSync('/proc/cpuinfo', 'utf8');
             let cont_array = content.split("\n");
             let serial_line = cont_array[cont_array.length-2];
             let serial = serial_line.split(":");*/
-            let device_id = 'BBP23';
+            let device_id = 'BBP24';
             let portS1 = new SerialPort(options.serialPort, {baudRate: options.baudRate, autoOpen: false, lock: false});
             portS1.open(function (err) {
                 if (err) {
@@ -61,6 +68,8 @@ module.exports = (SerialPort, nmea, net, fs, Readline, scServer) => {
                             'longitude': gprmc.loc.geojson.coordinates[0],
                             'speed': gprmc.speed.kmh
                         };
+                        let values = [response.latitude, response.longitude, response.speed, moment.utc().millisecond(), moment.utc().millisecond()];
+                        self.saveOfflineData(values);
                         let buffer = Buffer.from(JSON.stringify(response));
                         if (client.writable) {
                             client.write(buffer);

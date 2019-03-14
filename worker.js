cfe9b49960aa5704af402c1842d2b5bdeb0c9ff4
Alejandro <alecoexposito@gmail.com>
2019-03-14 16:06:46 -0400

    adding functionalities for download part of video

--- worker.js
+++ worker.js
@@ -57,7 +57,7 @@ class Worker extends SCWorker {
             console.log("a guardar en server: ", {deviceModel: 'BB', gpsData: response});
         });
 
-        db.run('update info_data set is_offline = 0', values, function(err) {
+        _this.db.run('update info_data set is_offline = 0', values, function(err) {
             if(err) {
                 return console.log(console.log(err.message));
             }

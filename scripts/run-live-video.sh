28dfd78e722fb8b2315a32bab69bbe4f9eec0e5f
Alejandro <alecoexposito@gmail.com>
2019-06-15 17:16:49 -0400

    adding support for obd

--- scripts/run-live-video.sh
+++ scripts/run-live-video.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-gst-launch-1.0 rtspsrc location="rtsp://192.168.1.5:554/user=admin&password=&channel=1&stream=1.sdp" latency=0 ! decodebin ! videorate ! video/x-raw,framerate=5/1 ! jpegenc ! multifilesink location=/home/zurikato/camera-local/camera.jpg
-wait
-echo "process is over"
+while [ $SECONDS -le 3 ]
+do
+    SECONDS=0
+    gst-launch-1.0 rtspsrc location="rtsp://192.168.1.5:554/user=admin&password=&channel=1&stream=1.sdp" latency=0 ! decodebin ! videorate ! video/x-raw,framerate=5/1 ! jpegenc ! multifilesink location=/home/zurikato/camera-local/camera.jpg
+    wait
+    echo "seconds passed: $SECONDS"
+done
+
+echo "salio del ciclo, seconds passed: $SECONDS"

43ecfc85aa43a820201e64ceab8fdb3e025fda5c
Alejandro <alecoexposito@gmail.com>
2019-06-20 16:20:07 -0400

    adding support for obd

--- worker.js
+++ worker.js
@@ -181,7 +181,7 @@ class Worker extends SCWorker {
                             _this.livePid = null;
                             clearInterval(interval);
                         }
-                    }, 3000)
+                    }, 12000)
 
                     // vcommand.kill("SIGKILL");
                 } else if(data.type == "start-video-backup") {

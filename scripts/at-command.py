13cdf1f46f9076c03c6474d298f38f661cdbb8d0
Alejandro <alecoexposito@gmail.com>
2019-09-30 11:24:40 -0400

    python script to send AT commands

---
+++ scripts/at-command.py
@@ -0,0 +1,13 @@
+import serial
+import sys
+with serial.serial_for_url('/dev/ttyUSB2', 115200, timeout=4) as s:
+    print("comando a correr: " + sys.argv[1])
+    s.write(str(sys.argv[1]) + '\r\n')
+    line = s.readline()
+    print(line)
+    line = s.readline()
+    print(line)
+    line = s.readline()
+    print(line)
+    line = s.readline()
+    print(line)

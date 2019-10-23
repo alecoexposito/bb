218fc0c9ba3f7dec2d8944f9932a60c3552baa30
Alejandro <alecoexposito@gmail.com>
2019-10-23 11:32:23 -0400

    fixing green images

--- scripts/run-live-video.sh
+++ scripts/run-live-video.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while [ $SECONDS -le 3 ]
+while [ $SECONDS -le 10 ]
 do
     SECONDS=0
 #    gst-launch-1.0 rtspsrc location="rtsp://192.168.1.5:554/user=admin&password=&channel=1&stream=1.sdp" latency=0 ! decodebin ! videorate ! video/x-raw,framerate=5/1 ! jpegenc ! multifilesink location=/home/zurikato/camera-local/camera.jpg
@@ -10,3 +10,4 @@ do
 done
 
 echo "salio del ciclo, seconds passed: $SECONDS"
+

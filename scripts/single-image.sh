a13d7f274d8b812eddf60f4f546a092ccb4b5d1d
Alejandro <alecoexposito@gmail.com>
2019-11-25 11:09:12 -0500

    camera autoplay functionality

--- scripts/single-image.sh
+++ scripts/single-image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -i "$1" -q:v 2 -y -vframes 1 /home/zurikato/camera-local/single-camera.jpg
+ffmpeg -rtsp_transport tcp -i "$1" -q:v 2 -y -vframes 1 /home/zurikato/camera-local/single-camera.jpg

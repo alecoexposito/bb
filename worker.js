ed562dddcbf79e16a73a2ea90828ad24910e3b39
Alejandro <alecoexposito@gmail.com>
2020-02-10 14:36:08 -0500

    fixing reconnection error

--- worker.js
+++ worker.js
@@ -492,7 +492,7 @@ class Worker extends SCWorker {
         });
         this.loadAutoplayCameras();
 
-        var vpnChannel = socket.subscribe('vpn_' + process.env.DEVICE_ID + '_channel');
+        var vpnChannel = this.socket.subscribe('vpn_' + process.env.DEVICE_ID + '_channel');
         vpnChannel.watch(function(data) {
             console.log("canal vpn: ", data);
             _this.runCommand('sudo', [

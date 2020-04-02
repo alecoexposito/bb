5c8525c5ffd3c3dc1aacd25b74da823f3096c77c
Alejandro <alecoexposito@gmail.com>
2020-04-02 18:34:47 -0400

    fixing reconnection error

--- BBCtrl.js
+++ BBCtrl.js
@@ -39,7 +39,9 @@ module.exports = (SerialPort, nmea, net, fs, Readline, scServer) => {
 
             tcpClient.on("error", function() {
                 console.log("error conectandose al tcp");
-                tcpClient.connect(2947, '127.0.0.1');
+                setTimeout(function(){
+                    tcpClient.connect(2947, '127.0.0.1');
+                }, 3000);
             })
 
 

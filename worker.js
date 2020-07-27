7bc110ed265bcd39576aaf0daff500e6287d63df
Alejandro <alecoexposito@gmail.com>
2020-07-27 13:26:59 -0400

    fixing reconnection error

--- worker.js
+++ worker.js
@@ -764,6 +764,7 @@ class Worker extends SCWorker {
         let lastMarkedDate = null;
         lineReader2.on('line', (line) => {
             noFileFound = false;
+            console.log("line: ", line);
             if (line.startsWith("#") || line.startsWith(" ")) {
                 lastUtilityLine = line;
             } else {

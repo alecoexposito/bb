24e04ff4600787f51f3569e7dd7fa48e11a3a599
Alejandro <alecoexposito@gmail.com>
2020-07-27 12:15:19 -0400

    fix

--- worker.js
+++ worker.js
@@ -361,6 +361,7 @@ class Worker extends SCWorker {
 
 
         cameraChannel.watch(function (data) {
+            console.log("ALGO ENTRO EN EL CAMERA CHANNEL: ", data);
             if (data.id == process.env.DEVICE_ID) {
                 if (data.type == "start-streaming") {
                     console.log("AAAAAAAAAAAAAAAAAAAAAA--------------received from web:------------AAAAAAAAAAAAAAA ", data);
